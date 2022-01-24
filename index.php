42c1a9ee97be56dd9a6715c9aa4a0e3a01ca0015
Peter <peterchen2017125@gmail.com>
2022-01-24 15:59:34 +0800

    01

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $a = 100;// 不可數字開頭，大小寫有別，符號只可使用_
+    echo $a;
+
+    define("USER","admin"); //常數
+    echo USER;
+
+    /* 
+        資料型別
+        字串 string
+        整數 int
+        浮點數 float
+        布林 boolean
+    */
+?>
