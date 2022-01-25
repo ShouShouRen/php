4ff2ce9a51b60bfa3e40e4e47234767bbbf479e2
Peter <peterchen2017125@gmail.com>
2022-01-25 16:49:33 +0800

    01

--- form/form.php
+++ form/form.php
@@ -8,8 +8,25 @@
 </head>
 <body>
     <form action="res.php" method="POST">
-        <input type="text" name="user">
-        <input type="text" name="mail">
+        <input type="text" name="user"><br>
+        <input type="text" name="mail"><br>
+        <input type="text" name="phone"><br>
+        <div>
+            <input type="radio" name="gender" value="男">男
+            <input type="radio" name="gender" value="女">女
+        </div>
+        <div>
+            <select name="edu" id="">
+                <option value="國小">國小</option>
+                <option value="國中">國中</option>
+                <option value="高中職">高中職</option>
+                <option value="大專院校">大專院校</option>
+                <option value="研究所以上">研究所以上</option>
+            </select>
+        </div>
+        <div>
+            <textarea name="content" id="" cols="30" rows="10"></textarea>
+        </div>
         <input type="submit" value="送出">
     </form>
 </body>
--- form/res.php
+++ form/res.php
@@ -1,7 +1,15 @@
 <?php
     // echo $_POST;
     // var_dump($_POST);
-    echo $_POST["user"];
-    echo "<br>";
-    echo $_POST["mail"];
+    // echo $_POST["user"];
+    // echo "<br>";
+    // echo $_POST["mail"];
+    // echo "<br>";
+    // echo $_POST["phone"];
+    // extract($_POST);
+    // echo $user;
+    // echo $mail;
+    // echo $phone;
+    extract($_POST);//一開始傳送為關聯陣列使用extract分為變數
+    echo $content;
 ?>
