0b32fa30bcb6f9050ca5a80190aac9cdca2b0a42
Peter <peterchen2017125@gmail.com>
2022-01-25 12:08:34 +0800

    01

---
+++ form/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="res.php" method="POST">
+        <input type="text" name="user">
+        <input type="text" name="mail">
+        <input type="submit" value="送出">
+    </form>
+</body>
+</html>
---
+++ form/res.php
@@ -0,0 +1,7 @@
+<?php
+    // echo $_POST;
+    // var_dump($_POST);
+    echo $_POST["user"];
+    echo "<br>";
+    echo $_POST["mail"];
+?>
