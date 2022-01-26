688fef99186c2d276118c87c182ecb611557814f
Peter <peterchen2017125@gmail.com>
2022-01-26 16:57:42 +0800

    01

--- database/conn.php
+++ database/conn.php
@@ -4,7 +4,7 @@
     $db_pw = "admin";
     $db_name = "lccnet";
 
-    $conn = @mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('連線錯誤');
+    $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('連線錯誤');
 
     mysqli_query($conn,"SET NAMES UTF8MB4");//執行資料庫語法
 ?>
--- database/index.php
+++ database/index.php
@@ -38,6 +38,9 @@
     </style>
 </head>
 <body>
+        <nav>
+            <a href="create.php">建立學員資料</a>
+        </nav>
     <table>
         <tr>
             <th>#</th>
@@ -48,20 +51,47 @@
             <th>學歷</th>
             <th>興趣</th>
             <th>備註</th>
+            <th>動作</th>
         </tr>
+        <?php while($row = mysqli_fetch_assoc($result)){ ?>
+            <tr>
+                <td><?php echo $row["id"]?></td>
+                <td><?php echo $row["name"]?></td>
+                <td><?php echo $row["phone"]?></td>
+                <td><?php echo $row["mail"]?></td>
+                <td><?php echo $row["gender"]?></td>
+                <td><?php echo $row["edu"]?></td>
+                <td><?php echo $row["interest"]?></td>
+                <td><?php echo $row["content"]?></td>
+                <td>
+                    <form action="delete.php" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $row["id"];?>">
+                        <input type="submit" value="刪除">
+                    </form>
+                    <a href="edit.php?id=<?php echo $row["id"];?>">編輯</a>
+                </td>
+            </tr>
+        <?php } ?>
+
+
+
+        <!-- 兩種寫法結果相依 -->
+
+
+
         <?php
-            while($row = mysqli_fetch_assoc($result)){
-                echo "<tr>";
-                echo "<td>".$row["id"]."</td>";
-                echo "<td>".$row["name"]."</td>";
-                echo "<td>".$row["phone"]."</td>";
-                echo "<td>".$row["mail"]."</td>";
-                echo "<td>".$row["gender"]."</td>";
-                echo "<td>".$row["edu"]."</td>";
-                echo "<td>".$row["interest"]."</td>";
-                echo "<td>".$row["content"]."</td>";
-                echo "</td>";
-            }
+            // while($row = mysqli_fetch_assoc($result)){
+            //     echo "<tr>";
+            //     echo "<td>".$row["id"]."</td>";
+            //     echo "<td>".$row["name"]."</td>";
+            //     echo "<td>".$row["phone"]."</td>";
+            //     echo "<td>".$row["mail"]."</td>";
+            //     echo "<td>".$row["gender"]."</td>";
+            //     echo "<td>".$row["edu"]."</td>";
+            //     echo "<td>".$row["interest"]."</td>";
+            //     echo "<td>".$row["content"]."</td>";
+            //     echo "</td>";
+            // }
         ?>
     </table>
 </body>
