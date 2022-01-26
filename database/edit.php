069369d1ebe05f84b690961363a9d95ae73f075a
Peter <peterchen2017125@gmail.com>
2022-01-26 16:58:13 +0800

    01

--- database/edit.php
+++ database/edit.php
@@ -15,7 +15,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="store.php" method="POST">
+    <form action="update.php" method="POST">
         <div>
             <label for="name">姓名</label>
             <input type="text" name="name" value="<?php echo $row["name"];?>">
@@ -30,30 +30,35 @@
         </div>
         <div>
             <label for="gender">性別</label>
-            <input type="radio" name="gender" value="男">男
-            <input type="radio" name="gender" value="女">女
+            <input type="radio" name="gender" value="男" <?php if($row["gender"]=="男"){echo "checked";}?>>男
+            <input type="radio" name="gender" value="女" <?php if($row["gender"]=="女"){echo "checked";}?>>女
         </div>
         <div>
             <label for="edu">學歷</label>
             <select name="edu" id="">
-                <option value="國小">國小</option>
-                <option value="國中">國中</option>
-                <option value="高中職">高中職</option>
-                <option value="大專院校">大專院校</option>
-                <option value="研究所以上">研究所以上</option>
+                <option value="國小" <?php echo $row["edu"]=="國小" ? "selected" : "";?>>國小</option>
+                <option value="國中" <?php echo $row["edu"]=="國中" ? "selected" : "";?>>國中</option>
+                <option value="高中職" <?php echo $row["edu"]=="高中職" ? "selected" : "";?>>高中職</option>
+                <option value="大專院校" <?php echo $row["edu"]=="大專院校" ? "selected" : "";?>>大專院校</option>
+                <option value="研究所以上" <?php echo $row["edu"]=="研究所以上" ? "selected" : "";?>>研究所以上</option>
             </select>
         </div>
         <div>
+            <?php
+                $interest = explode(",",$row["interest"]);//先將字串轉為陣列再用array去判斷是存在
+
+            ?>
             <label for="interest">興趣</label>
-            <input type="checkbox" name="interest[]" value="吃">吃
-            <input type="checkbox" name="interest[]" value="喝">喝
-            <input type="checkbox" name="interest[]" value="玩">玩
-            <input type="checkbox" name="interest[]" value="樂">樂
+            <input type="checkbox" name="interest[]" value="吃" <?php echo in_array("吃",$interest) ? "checked" :"";?>>吃
+            <input type="checkbox" name="interest[]" value="喝" <?php echo in_array("喝",$interest) ? "checked" :"";?>>喝
+            <input type="checkbox" name="interest[]" value="玩" <?php echo in_array("玩",$interest) ? "checked" :"";?>>玩
+            <input type="checkbox" name="interest[]" value="樂" <?php echo in_array("樂",$interest) ? "checked" :"";?>>樂
         </div>
         <div>
             <label for="content">備註</label>
             <textarea name="content" id="" cols="30" rows="10"><?php echo $row["content"];?></textarea>
         </div>
+        <input type="hidden" name="id" value="<?php echo $row["id"];?>">
         <input type="submit" value="送出">
     </form>
 </body>
